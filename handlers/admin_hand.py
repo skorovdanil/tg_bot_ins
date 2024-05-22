a15707efb148dc3e45de3178516cf2363b51ab51
skorovdanil <skorovdanil@mail.ru>
2024-05-22 17:12:56 +0300

    main

--- handlers/admin_hand.py
+++ handlers/admin_hand.py
@@ -3,7 +3,6 @@ from aiogram.filters import Command
 from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
-from aiogram.utils.deep_linking import create_start_link, decode_payload
 from dotenv import load_dotenv
 load_dotenv()
 import os
